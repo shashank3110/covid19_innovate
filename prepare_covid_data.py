d83cf40bd33d5aefd0c5c191d7073117b3f16c2f
shashank3110 <shashank311095@gmail.com>
2020-03-27 00:17:43 +0100

    india map with state demarcations no of cases on map visualized for now ladakh combined with jk

--- prepare_covid_data.py
+++ prepare_covid_data.py
@@ -5,22 +5,26 @@ from collections import defaultdict
 import folium
 import json
 import pandas as pd
+#import plotly.express as px
+
 def get_state_data():
     response = requests.get('https://api.rootnet.in/covid19-in/stats/daily')
 
     response_dict = response.json()
     states = defaultdict(dict)
+    
     status = response_dict['success']
     cases = []#defaultdict()
     print(f'Data Pull status={status}')
+
     state_list=['Sikkim','Andhra Pradesh', 'Bihar', 'Chhattisgarh','Arunachal Pradesh',\
          'Delhi', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Karnataka', \
         'Kerala', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Mizoram', \
         'Odisha', 'Puducherry', 'Punjab', 'Rajasthan', 'Tamil Nadu',\
-         'Chandigarh', 'Jammu and Kashmir', 'Ladakh', 'Uttar Pradesh',\
+         'Chandigarh', 'Jammu and Kashmir',  'Uttar Pradesh',\
         'Uttarakhand', 'West Bengal','Telengana','Tripura','Assam',
         'Goa','Nagaland','Lakshadweep','Dadra and Nagar Haveli', 'Daman and Diu',
-        'Jharkhand','Andaman and Nicobar Islands']
+        'Jharkhand','Meghalaya','Andaman and Nicobar Islands']
     try:
         daily_data = response_dict['data']
 
@@ -36,44 +40,46 @@ def get_state_data():
         latest_date = latest_data['day']
         latest_summary = latest_data['summary']
         #print(latest_date,latest_summary,latest_regional_data)
+        const=0
         for item  in latest_regional_data:
             if item['loc']=='Ladakh':
+                const = item['confirmedCasesIndian']+item['confirmedCasesForeign']+item['discharged']+item['deaths']
                 continue
-            k = item.pop('loc')
-            states[k] = item
-            total_cases = item['confirmedCasesIndian']+item['confirmedCasesForeign']+item['discharged']+item['deaths']
-            cases.append( {'State':k ,'Total_Cases': total_cases})
-        #print(states)
+            else:
+                k = item.pop('loc')
+                states[k] = item
+                total_cases = item['confirmedCasesIndian']+item['confirmedCasesForeign']+item['discharged']+item['deaths']
+                cases.append( {'State':k ,'Total_Cases': total_cases})
+                
         state_geo_data=json.load(open('/home/shashanks/git_code/covid19_innovate/indian_states.json'))
-        #geo_data = json.loads('/home/shashanks/git_code/covid19_innovate/indian_states.json')
-        #print(state_data)
-        #pd.read_json('/home/shashanks/git_code/covid19_innovate/indian_states.json')
-        m = folium.Map([20.5937, 78.9629],zoom_start=4.5)
-        #print(cases)
+        
+        m = folium.Map([20.5937, 78.9629],zoom_start=4.35) # centered on coordinates of india
+       
         
         
         
         #print(state_geo_data['features'].keys())
-        for s in state_list:
-            if s not in states.keys():
-                cases.append( {'State':s ,'Total_Cases': 0})
-            states[s] = {'confirmedCasesIndian': 0, 'confirmedCasesForeign': 0, 'discharged': 0, 'deaths': 0}
+        for st in state_list:
+            if st not in states.keys():
+                cases.append( {'State':st ,'Total_Cases': 0})
+            states[st] = {'confirmedCasesIndian': 0, 'confirmedCasesForeign': 0, 'discharged': 0, 'deaths': 0}
             
         
         state_data = pd.DataFrame(cases,columns=['State','Total_Cases'])
         state_data.sort_values(by=['State'],inplace=True)
-        print(state_data.head())
-        print(state_data['State'],len(state_data['State']))
+        
+        state_data.loc[state_data['State']=='Jammu and Kashmir','Total_Cases']+=const
+        print(state_data)
+       
         print('########################################')
         print(states.keys())
         print(len(states.keys()))
-        #state_geo_data_df = pd.DataFrame(state_geo_data)
+        
         print(type(state_geo_data))
-        #print(state_geo_data['features'][0])
-        #print(state_geo_data_df.head())
+      
         for i,_ in enumerate(state_geo_data['features']):
             state_geo_data['features'][i]['id'] = state_geo_data['features'][i]['properties']['NAME_1']
-            state_data['State'][i]=state_geo_data['features'][i]['id']
+            #state_data['State'][i]=state_geo_data['features'][i]['id']
             print(state_geo_data['features'][i]['id'])
         #state_data
         folium.Choropleth(
@@ -81,13 +87,14 @@ def get_state_data():
     name='choropleth',
     data=state_data,
     columns=['State', 'Total_Cases'],
-    key_on='features.id',#'features.properties.NAME_1',
-    fill_color='YlGn',
+    key_on= 'feature.properties.NAME_1',#'features.id',#'features.properties.NAME_1',
+    fill_color='YlOrRd',
     fill_opacity=0.7,
     line_opacity=0.9,
     legend_name='Total Covid 19 cases in India'
 ).add_to(m)
         m.save('Map.html')
+        #print(state_geo_data)
         #response= requests.get('https://github.com/geohacker/india/blob/master/state/india_telengana.geojson')
         #counties = response.json()#json.loads(response)
     except Exception as e:
